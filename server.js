723d1435787ba26aa1d2d6ee0741a75f9baa9157
Bijo Abraham <bijoabraham@gmail.com>
2018-04-01 11:01:18 -0500

    Templating using Handlebar

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 
+//handle bar for templating 
+const hbs = require("hbs") ;
+
 //Initialize server app
 app = express();
+
+//set handle bar hbs as view engine
+app.set("view engine","hbs");
 
 //express middlewear to control express behaviour - here to serve static page from public folder
 app.use(express.static(__dirname+'/public'))
@@ -9,6 +15,13 @@ app.use(express.static(__dirname+'/public'))
 //Code to handle req and response
 app.get("/",(req,res)=>{
     res.send("Hello Word");
+});
+
+//render tempalte on request
+//passing data to template
+app.get("/about",(req,res)=>{
+    //the tempalte is always searched in the views folder
+    res.render("about",{content:"dynamic data on a template render"});
 });
 
 //start the server
